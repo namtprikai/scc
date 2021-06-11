39f4baa0f2d650465a77e6acd5b835cd535df1e4
kotaroyamame <yamame@fi.chu.jp>
2021-06-11 23:39:50 +0900

    add Answer comp

---
+++ src/core/components/Answer/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch, Prop, Emit } from 'vue-property-decorator';
+
+@Component
+export default class AnswerComp extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
