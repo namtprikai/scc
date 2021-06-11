50cf871610746b93b7fbfb7447c61fbf54b1423b
kotaroyamame <yamame@fi.chu.jp>
2021-06-11 12:56:44 +0900

    add role

---
+++ mock/answer_roles.ts
@@ -0,0 +1,7 @@
+export const answerRoles = [
+	{
+		id:0,
+		role_id:0,
+		answer_id:0
+	}
+];
--- mock/roles.ts
+++ mock/roles.ts
@@ -1,6 +1,11 @@
 import {IRoleData,IAPIResponce} from "../src/core/api/types";
 import { Response, Request } from 'express';
-export const roles:Array<IRoleData> = [];
+export const roles:Array<IRoleData> = [
+	{
+		id:0,
+		label:"ゲスト",
+	}
+];
 
 export const getRoleList = (req: Request, res: IAPIResponce)=> {
 
