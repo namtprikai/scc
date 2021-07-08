79caf969d3ce6de80a6d484af315bdf20861d992
kotaroyamame <yamame@fi.chu.jp>
2021-07-08 22:48:55 +0900

    カテゴリの権限ロジック

--- mock/category.ts
+++ mock/category.ts
@@ -1,7 +1,9 @@
 import faker from "faker";
 import { Response, Request } from "express";
-import { ICategoryData } from "../src/core/api/types";
+import { ICategoryData,IProductData } from "../src/core/api/types";
 import { IAPIResponce } from "../src/core/api/types";
+import { userProducts } from "./user_products";
+import { productions } from "./products";
 import { secureObjectCreateByAdmin } from "./security";
 // import { } from '';
 const categorys: Array<ICategoryData> = [
@@ -14,21 +16,21 @@ const categorys: Array<ICategoryData> = [
 		modified: new Date(),
 	},
 ];
-// const Categorys = secureObjectCreateByAdmin<ICategoryData>(() => categorys, (c) => {
-// 	const products: Array<IProductData> = [];
-// 	for (const up of userProducts) {
-// 		if (u.id === up.user_id) {
-// 			for (const production of productions) {
-// 				if (up.product_id === production.id) {
-// 					products.push(production);
-// 					break;
-// 				}
-// 			}
-// 		}
+const Categorys = secureObjectCreateByAdmin<ICategoryData>(() => categorys, (c) => {
+	const products: Array<IProductData> = [];
+	for (const up of userProducts) {
+		if (c.id === up.user_id) {
+			for (const production of productions) {
+				if (up.product_id === production.id) {
+					products.push(production);
+					break;
+				}
+			}
+		}
 
-// 	}
-// 	return products;
-// });
+	}
+	return products;
+});
 export const getCategoryList = (req: Request, res: IAPIResponce): Response => {
 	const { parent_id } = req.query;
 
