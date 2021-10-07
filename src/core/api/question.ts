b5adbb935c83f745768c0a82a01a6d766dc2e7f8
kotaroyamame <yamame@fi.chu.jp>
2021-10-07 14:58:48 +0900

    add Question api

--- src/core/api/question.ts
+++ src/core/api/question.ts
@@ -1,4 +1,5 @@
 import { AjaxService } from "@/services/ajax";
+import {IQuestionData} from "@/api/types";
 export namespace Question{
 	export const getList = async ()=>{
 		const { data }: any = await AjaxService.ajax.http({
@@ -9,7 +10,31 @@ export namespace Question{
 		console.log(data);
 		return data;
 	};
-	export const post = ()=>{};
-	export const patch = ()=>{};
-	export const deleteObject = ()=>{};
+	export const post = async(input:IQuestionData)=>{
+		const { data }: any = await AjaxService.ajax.http({
+			url: `question/`,
+			method: "post",
+			data: input,
+		});
+		console.log(data);
+		return data;
+	};
+	export const patch = async(id:number,input:any)=>{
+		const { data }: any = await AjaxService.ajax.http({
+			url: `question/{${id}}/`,
+			method: "patch",
+			data: input,
+		});
+		console.log(data);
+		return data;
+	};
+	export const deleteObject = async (id:number)=>{
+		const { data }: any = await AjaxService.ajax.http({
+			url: `question/{${id}}/`,
+			method: "post",
+			data: {},
+		});
+		console.log(data);
+		return data;
+	};
 }
