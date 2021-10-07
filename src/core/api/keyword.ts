d97d979c4e6880052dc716cbad00814741c5afe3
kotaroyamame <yamame@fi.chu.jp>
2021-10-07 19:07:39 +0900

    add: api. keyword and policygroup

--- src/core/api/keyword.ts
+++ src/core/api/keyword.ts
@@ -1,8 +1,43 @@
 import { AjaxService } from "@/services/ajax";
+import { IKeywordData } from "./types";
 
 namespace Keyword{
+	const URL = "api/keyword/";
 	export const get = ()=>{};
-	export const post = ()=>{};
-	export const patch = ()=>{};
-	export const deleteObject = ()=>{};
+	export const getList = async()=>{
+		const { data }: any = await AjaxService.ajax.http({
+			url: `${URL}`,
+			method: "get",
+			params: {},
+		});
+		console.log(data);
+		return data;
+	};
+	export const post = async (input:IKeywordData)=>{
+		const { data }: any = await AjaxService.ajax.http({
+			url: `${URL}`,
+			method: "post",
+			data: input,
+		});
+		console.log(data);
+		return data;
+	};
+	export const patch = async(id:number,input:any)=>{
+		const { data }: any = await AjaxService.ajax.http({
+			url:  `${URL}/{${id}}/`,
+			method: "patch",
+			data: input,
+		});
+		console.log(data);
+		return data;
+	};
+	export const deleteObject = async (id:number)=>{
+		const { data }: any = await AjaxService.ajax.http({
+			url: `${URL}/{${id}}/`,
+			method: "post",
+			data: {},
+		});
+		console.log(data);
+		return data;
+	};
 }
--- src/core/api/policygroup.ts
+++ src/core/api/policygroup.ts
@@ -22,6 +22,22 @@ namespace PolycyGroup{
 		console.log(data);
 		return data;
 	};
-	export const patch = ()=>{};
-	export const deleteObject = ()=>{};
+	export const patch = async(id:number,input:any)=>{
+		const { data }: any = await AjaxService.ajax.http({
+			url:  `${URL}/{${id}}/`,
+			method: "patch",
+			data: input,
+		});
+		console.log(data);
+		return data;
+	};
+	export const deleteObject = async (id:number)=>{
+		const { data }: any = await AjaxService.ajax.http({
+			url: `${URL}/{${id}}/`,
+			method: "post",
+			data: {},
+		});
+		console.log(data);
+		return data;
+	};
 }
