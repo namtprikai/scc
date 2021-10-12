bb5de1ebdd5cc5f503061c236233d317f5f71759
kotaroyamame <yamame@fi.chu.jp>
2021-10-12 16:00:46 +0900

    add answercondition

--- mock/answer_conditions.ts
+++ mock/answer_conditions.ts
@@ -1,4 +1,13 @@
-export const answerConditions = [
+import { ICategory_Question, ISAIAPIData } from '../src/core/api/types';
+import { IAdminData, ICrossReferenceTable } from "../src/core/api/types";
+import { CrossReferenceTable } from "./utils";
+interface IAnswerCondition extends ICrossReferenceTable{
+	// question_id:number;
+	// group_id:number;
+	// keyword_id:number;
+}
+
+export let answerConditions:Array<IAnswerCondition> = [
 	{
 		id: 0,
 		answer_id: 0,
@@ -10,3 +19,23 @@ export const answerConditions = [
 		condition_id: 4,
 	},
 ];
+class AnswerCondition extends CrossReferenceTable<IAnswerCondition>{
+	public getConditionByAnswerId(answer_id: number,table:Array<ICrossReferenceTable>) {
+		return this.getAByB(answer_id,table);
+	}
+	public deletebyConditionId(condition_id: number,table:Array<ICrossReferenceTable>){
+		this.deleteByBId(condition_id,table);
+	}
+	public deletebyAnswerId(answer_id: number,table:Array<ICrossReferenceTable>){
+		this.deleteByAId(answer_id,table);
+	}
+	public addConditionByAnswerId(answer_id: number,conditionIdList:Array<number>,table:Array<ICrossReferenceTable>):Array<number> {
+		const addIdList = [];
+		for(const conditionId of conditionIdList){
+			this.add(answer_id,conditionId,table);
+			addIdList.push(conditionId);
+		}
+		return addIdList;
+	}
+}
+const answerConditionModel = new AnswerCondition("answer_id","condition_id");
