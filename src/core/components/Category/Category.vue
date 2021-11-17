922c1fc0d04de9267184e0d315bf5fe942c9923e
Kotaro <info@iid.systems>
2021-11-17 14:49:57 +0900

    add

---
+++ src/core/components/Category/Category.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch, Prop } from 'vue-property-decorator';
+@Component({
+	components: {},
+})
+export default class CategoryTemp extends Vue {
+@Prop()
+title!:string;
+}
+</script>
