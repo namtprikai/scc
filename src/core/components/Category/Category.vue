6f88e1ba2823f6f569abf0bf05fbe7e665643d43
Kotaro <info@iid.systems>
2021-11-17 15:30:19 +0900

    add tree

--- src/core/components/Category/Category.vue
+++ src/core/components/Category/Category.vue
@@ -1,16 +1,31 @@
 <template>
 	<div>
-
+		<vue-tree
+			style="width: 800px; height: 600px; border: 1px solid gray;"
+			:dataset="sampleData"
+			:config="treeConfig"
+		></vue-tree>
 	</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch, Prop } from 'vue-property-decorator';
+// @ts-ignore
+import VueTree from '@ssthouse/vue-tree-chart';
+// Vue.component('vue-tree', VueTree)
 @Component({
-	components: {},
+	components: { VueTree },
 })
 export default class CategoryTemp extends Vue {
-@Prop()
-title!:string;
+	sampleData = {
+		value: '1',
+		children: [
+			{ value: '2', children: [{ value: '4' }, { value: '5' }] },
+			{ value: '3' }
+		]
+	}
+	treeConfig = { nodeWidth: 120, nodeHeight: 80, levelHeight: 200 }
+	@Prop()
+	title!: string;
 }
 </script>
