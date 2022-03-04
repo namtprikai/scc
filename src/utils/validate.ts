bb13292f79b783ed3f93cc2b9fc0a02779e0294d
Thái Nguyễn Tấn <thainguyentan@Thais-MacBook-Pro.local>
2022-03-04 17:29:51 +0700

    update message and validation common method

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,3 +1,6 @@
+import i18n from '@/lang'
+import { ValidationType } from '@/utils/request'
+import { TranslateResult } from 'vue-i18n'
 export const isValidUsername = (str: string) => {
   return str.trim() !== ''
 }
@@ -14,4 +17,32 @@ export const isArray = (arg: any) => {
 export const isValidURL = (url: string) => {
   const reg = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/
   return reg.test(url)
+}
+
+// type: validation type
+// name: name: validation property
+// value: example: length, min value, max value ...
+export const getValidationMessage = (type:ValidationType, name:string|null|undefined|TranslateResult = null, value:number|null = null):string|TranslateResult => {
+  switch (type) {
+    case ValidationType.Empty:
+      return i18n.t('validError.required', { _field_: name })
+    case ValidationType.Min:
+      return i18n.t('validError.min', { _field_: name, length: value })
+    case ValidationType.Max:
+      return i18n.t('validError.max', { _field_: name, length: value })
+    case ValidationType.MinLength:
+      return i18n.t('validError.minLength', { _field_: name })
+    case ValidationType.MaxLength:
+      return i18n.t('validError.maxLength', { _field_: name })
+    case ValidationType.Pattern:
+      return i18n.t('validError.regex', { _field_: name })
+    case ValidationType.Exists:
+    case ValidationType.Role:
+    case ValidationType.Mismatch:
+    case ValidationType.Limit:
+    case ValidationType.Unique:
+    case ValidationType.Lock:
+    case ValidationType.Hierarchy:
+      return i18n.t(`validError.${type}`)
+  }
 }
