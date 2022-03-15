3e91d3fe4103c330704d9f394a33ebe2c70fcebd
toantczenken <toan-tc@zenkenplanet-vn.com>
2022-03-15 11:30:06 +0700

    Except show toast  notfound error

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -152,6 +152,10 @@ service.interceptors.response.use(
 
     // Forbidden, Not Found, Internal Server Error
     if ([403, 404, 500].includes(status)) {
+      // Except notfound error show message of active-admin screen
+      if (error.response.config.url?.includes('/admin/enabled/') && error.response.config.url.length > '/admin/enabled/'.length && status === 404) {
+        return Promise.reject(new Error(error.message))
+      }
       const errorCode = data?.errors && data?.errors[0]?.code
       if (errorCode && Object.values(APIErrorCode).includes(errorCode)) {
         Message({
