cd41ab73019950e071027b79cb76a399fbedb001
tlqbao <94093040+tlqbao@users.noreply.github.com>
2022-03-16 16:40:08 +0700

    Update index.vue

--- src/components/Notification/index.vue
+++ src/components/Notification/index.vue
@@ -1,5 +1,5 @@
 <template>
- <a class="notf" href="#/notfication">
+ <a class="notfication" href="/notfication">
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
       <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
     </svg>
@@ -17,8 +17,8 @@ export default class extends Vue {
 
 }
 </script>
-<style>
-.notf{
+<style lang="scss" scoped>
+.notfication{
     position: absolute;
     top: 50%;
     transform: translate(0, -50%);
@@ -26,3 +26,4 @@ export default class extends Vue {
 }
 
 </style>
+
