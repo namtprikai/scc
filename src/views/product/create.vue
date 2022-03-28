fd334589f8cfea5cb5cd9b7113ae4057fadfc9f8
Linh <linhbachvanhoai@Linhs-MacBook-Pro.local>
2022-03-28 10:12:56 +0700

    refactor create product

--- src/views/product/create.vue
+++ src/views/product/create.vue
@@ -1,46 +1,50 @@
 <template>
-  <div class="create-product-container">
-    <el-form
-      class="form-product"
-      ref="createForm"
-      label-position="top"
-      :rules="createRules"
-      :model="createForm"
-    >
-      <el-form-item label="temp" :error="createProductError.productName" prop="productName">
-        <span slot="label">{{ $t('labelText.productName') }}</span>
-        <el-input v-model="createForm.productName" tabindex="1" autofocus></el-input>
-      </el-form-item>
-      <el-form-item label="temp" :error="createProductError.maxFailureCountUser">
-        <span slot="label">{{ $t('labelText.maxFailureCountUser') }}</span>
-        <el-input type="number" v-model.number="createForm.maxFailureCountUser" tabindex="2"></el-input>
-      </el-form-item>
-      <el-form-item label="temp" :error="createProductError.maxFailureTimeUser">
-        <span slot="label">{{ $t('labelText.maxFailureTimeUser') }}</span>
-        <el-input type="number" v-model.number="createForm.maxFailureTimeUser" tabindex="3"></el-input>
-      </el-form-item>
-      <el-form-item label="temp" :error="createProductError.config">
-        <span slot="label">{{ $t('labelText.memo') }}</span>
-        <div class="json-editor">
-          <JsonEditor
-            :options="{
-              confirmText: $t('text.ok'),
-              cancelText: $t('text.cancel')
-            }"
-            :objData="createForm.config"
-            v-model="createForm.config"
-            tabindex="4"
-          ></JsonEditor>
-        </div>
-      </el-form-item>
-      <el-form-item>
-        <el-button
-          type="primary"
-          @click="submit"
-          tabindex="5"
-        >{{ $t('text.submit') }}</el-button>
-      </el-form-item>
-    </el-form>
+  <div class="sm-container">
+    <el-card class="box-card">
+      <el-form
+        class="form-product"
+        ref="createForm"
+        label-position="top"
+        :rules="createRules"
+        :model="createForm"
+      >
+        <el-form-item :label="$t('labelText.productName')" :error="createProductError.productName" prop="name">
+          <el-input v-model="createForm.name" @blur="createForm.name = createForm.name.trim()" tabindex="1" autofocus></el-input>
+        </el-form-item>
+        <el-form-item :label="$t('labelText.maxFailureCountUser')" :error="createProductError.maxFailureCountUser">
+          <el-input type="number" v-model.number="createForm.maxFailureCountUser" tabindex="2"></el-input>
+        </el-form-item>
+        <el-form-item :label="$t('labelText.maxFailureTimeUser')" :error="createProductError.maxFailureTimeUser">
+          <el-input type="number" v-model.number="createForm.maxFailureTimeUser" tabindex="3"></el-input>
+        </el-form-item>
+        <el-form-item :label="$t('labelText.memo')" :error="createProductError.config">
+          <div class="json-editor">
+            <json-editor
+              :options="{
+                confirmText: $t('text.ok'),
+                cancelText: $t('text.cancel')
+              }"
+              :objData="createForm.config"
+              v-model="createForm.config"
+              tabindex="4"
+            ></json-editor>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="primary"
+            @click.native.prevent="confirmProductData"
+            tabindex="5"
+          >{{ $t('text.submit') }}</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <confirm-dialog
+      :dialogVisible.sync="confirmdialogVisible"
+      :confirmData="confirmData"
+      :title="$t('text.createScreenModalConfirmTitle')"
+      @ok="handleSubmit"
+    />
   </div>
 </template>
 
@@ -55,16 +59,20 @@ import { createProduct } from '@/api/production'
 import { ValidationError, ValidationType } from '@/utils/request'
 import { getValidationMessage } from '@/utils/validate'
 import { ProductErrorValue } from './product-error-value'
+import { mapKeys, snakeCase } from 'lodash'
+import ConfirmDialog from '@/components/ConfirmDialog/index.vue'
 
 @Component({
   name: 'CreateProduct',
   components: {
-    JsonEditor
+    JsonEditor,
+    ConfirmDialog
   }
 })
 
 export default class extends Vue {
-  public labelPosition = 'right'
+  confirmData: any = null
+  public confirmdialogVisible = false
   public createProductError: any = {
     productName: null,
     maxFailureCountUser: null,
@@ -73,15 +81,15 @@ export default class extends Vue {
   }
 
   public createForm = {
-    productName: '',
+    name: '',
     maxFailureCountUser: null,
     maxFailureTimeUser: null,
     config: {}
   }
 
   // createProduct rules
-  private createRules = {
-    productName: [
+  public createRules = {
+    name: [
       {
         required: true,
         message: getValidationMessage(ValidationType.Empty, this.$t('labelText.productName')),
@@ -100,114 +108,105 @@ export default class extends Vue {
     }
   }
 
-  public submit() {
+  public confirmProductData() {
+    (this.$refs.createForm as ElForm).validate(async(valid: boolean) => {
+      if (valid) {
+        this.confirmData = [
+          { key: this.$t('labelText.productName'), value: this.createForm.name },
+          { key: this.$t('labelText.maxFailureCountUser'), value: this.createForm.maxFailureCountUser },
+          { key: this.$t('labelText.maxFailureTimeUser'), value: this.createForm.maxFailureTimeUser },
+          {
+            key: this.$t('labelText.memo'),
+            value: JSON.stringify(this.createForm.config, undefined, 4),
+            type: 'json'
+          }
+        ]
+        this.confirmdialogVisible = true
+      } else {
+        return false
+      }
+    })
+  }
+
+  public handleSubmit() {
     (this.$refs.createForm as ElForm).validate(async(valid: boolean) => {
       if (valid) {
         this.resetMessageValidate()
-        // eslint-disable-next-line prefer-regex-literals
-        const name = this.createForm.productName.trim() === null ? '' : this.createForm.productName.trim().replace(new RegExp('<', 'g'), '&lt;').replace(new RegExp('>', 'g'), '&gt;')
-        this.$confirm(
-          `
-            <table class="message">
-                <tr>
-                    <td class="td-left">${this.$t('labelText.productName')}</td>
-                    <td class="td-center">:</td>
-                    <td class="td-right">
-                      <div class="max-size-height overflow-scroll">${name}</div>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="td-left">${this.$t('labelText.maxFailureCountUser')}</td>
-                    <td class="td-center">:</td>
-                    <td class="td-right">${this.createForm.maxFailureCountUser === null ? '' : this.createForm.maxFailureCountUser}</td>
-                </tr>
-                <tr>
-                    <td class="td-left">${this.$t('labelText.maxFailureTimeUser')}</td>
-                    <td class="td-center">:</td>
-                    <td class="td-right">${this.createForm.maxFailureTimeUser === null ? '' : this.createForm.maxFailureTimeUser}</td>
-                </tr>
-                <tr>
-                    <td class="td-left">${this.$t('labelText.memo')}</td>
-                    <td class="td-center">:</td>
-                    <td class="td-right"><pre class="max-size-height max-size-width margin-none"><xmp>${JSON.stringify(this.createForm.config, undefined, 4)}</xmp></pre></td>
-                </tr>
-            </table>
-          `,
-          this.$t('text.createScreenModalConfirmTitle') as string, {
-            confirmButtonText: this.$t('text.ok') as string,
-            cancelButtonText: this.$t('text.cancel') as string,
-            type: 'info',
-            dangerouslyUseHTMLString: true,
-            center: true,
-            customClass: 'popup-custom'
-          }
-        ).then(async() => {
-          try {
-            const dataPost = {
-              name: this.createForm.productName.trim() === '' ? null : this.createForm.productName.trim(),
-              config: this.createForm.config,
-              max_failure_count_user: this.createForm.maxFailureCountUser === '' ? null : this.createForm.maxFailureCountUser,
-              max_failure_time_user: this.createForm.maxFailureTimeUser === '' ? null : this.createForm.maxFailureTimeUser
-            }
-            try {
-              const { data } = await createProduct(dataPost)
-              if (data) {
-                // show modal create successfully
-                this.$alert(this.$t('message.productCreateSuccess') as string, '', {
-                  confirmButtonText: this.$t('text.ok') as string,
-                  type: 'success',
-                  center: true,
-                  callback: () => {
-                    // redirect to products list
-                    this.$router.push({
-                      // TODO: T.B.D This code will add next sprint
-                      path: '#'
-                    }).catch(err => {
-                      this.$message({
-                        message: err as string,
-                        type: 'error',
-                        duration: 5 * 1000
-                      })
-                    })
-                  }
+        const dataPost = {
+          name:
+            this.createForm.name.trim() === ''
+              ? null
+              : this.createForm.name.trim(),
+          config: this.createForm.config,
+          maxFailureCountUser:
+            this.createForm.maxFailureCountUser === ''
+              ? null
+              : this.createForm.maxFailureCountUser,
+          maxFailureTimeUser:
+            this.createForm.maxFailureTimeUser === ''
+              ? null
+              : this.createForm.maxFailureTimeUser
+        }
+        try {
+          const { data } = await createProduct(mapKeys(dataPost, (v, k) => snakeCase(k)))
+          if (data) {
+            this.$alert(this.$t('message.productCreateSuccess') as string, '', {
+              confirmButtonText: this.$t('text.ok') as string,
+              type: 'success',
+              center: true,
+              callback: () => {
+                // redirect to products list
+                this.$router.push({
+                  name: 'ListProduct'
+                }).catch(err => {
+                  this.$message({
+                    message: err as string,
+                    type: 'error',
+                    duration: 5 * 1000
+                  })
                 })
               }
-            } catch (err) {
-              // check if error 422
-              if (err instanceof ValidationError) {
-                const validationError = err as ValidationError
-                if (validationError.data?.length) {
-                  validationError.data.forEach((err) => {
-                    // get message error
-                    switch (err.value) {
-                      case ProductErrorValue.Name:
-                        this.createProductError.productName = getValidationMessage(err.type[0], this.$t('labelText.productName'))
-                        break
-                      case ProductErrorValue.MaxFailureCountUser:
-                        this.createProductError.maxFailureCountUser = getValidationMessage(err.type[0], this.$t('labelText.maxFailureCountUser'))
-                        break
-                      case ProductErrorValue.MaxFailureTimeUser:
-                        this.createProductError.maxFailureTimeUser = getValidationMessage(err.type[0], this.$t('labelText.maxFailureTimeUser'))
-                        break
-                      case ProductErrorValue.Config:
-                        this.createProductError.config = getValidationMessage(err.type[0], this.$t('labelText.memo'))
-                        break
-                      default:
-                        break
-                    }
-                  })
-                }
-              }
-            }
-          } catch (err) {
-            this.$message({
-              message: err as string,
-              type: 'error',
-              duration: 5 * 1000
             })
           }
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        }).catch(() => {})
+        } catch (err) {
+          // check if error 422
+          if (err instanceof ValidationError) {
+            const validationError = err as ValidationError
+            if (validationError.data?.length) {
+              validationError.data.forEach(err => {
+                // get message error
+                switch (err.value) {
+                  case ProductErrorValue.Name:
+                    this.createProductError.productName = getValidationMessage(
+                      err.type[0],
+                      this.$t('labelText.productName')
+                    )
+                    break
+                  case ProductErrorValue.MaxFailureCountUser:
+                    this.createProductError.maxFailureCountUser = getValidationMessage(
+                      err.type[0],
+                      this.$t('labelText.maxFailureCountUser')
+                    )
+                    break
+                  case ProductErrorValue.MaxFailureTimeUser:
+                    this.createProductError.maxFailureTimeUser = getValidationMessage(
+                      err.type[0],
+                      this.$t('labelText.maxFailureTimeUser')
+                    )
+                    break
+                  case ProductErrorValue.Config:
+                    this.createProductError.config = getValidationMessage(
+                      err.type[0],
+                      this.$t('labelText.memo')
+                    )
+                    break
+                  default:
+                    break
+                }
+              })
+            }
+          }
+        }
       } else {
         return false
       }
@@ -217,24 +216,12 @@ export default class extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.create-product-container {
-  padding: 15px;
-
-  .json-editor {
+.json-editor {
     padding: 10px 0;
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   }
 
-  ::v-deep .el-form-item__label {
-    line-height: 20px !important;
-  }
-
-  .form-product{
-    width: 70%;
-    margin: 20px auto;
-    @media only screen and (max-width: 767px) {
-      width: 100%;
-    }
-  }
+::v-deep .el-form-item__label {
+  line-height: 20px !important;
 }
 </style>
