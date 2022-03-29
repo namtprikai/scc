7d598a292486e0865db23da8c9c34a426d5803f4
Thái Nguyễn Tấn <thainguyentan@Thais-MacBook-Pro.local>
2022-03-29 14:44:02 +0700

    create user

--- src/api/types/response.d.ts
+++ src/api/types/response.d.ts
@@ -1,10 +1,20 @@
 /* eslint-disable camelcase */
-export interface ICreateAdminResponse{
-    id: number
-    name: string
-    email: string
-    is_mailauth_completed: boolean
-    is_master: boolean
-    is_enabled: boolean
-    is_lock: boolean
-  }
+export interface ICreateAdminResponse {
+  id: number
+  name: string
+  email: string
+  is_mailauth_completed: boolean
+  is_master: boolean
+  is_enabled: boolean
+  is_lock: boolean
+}
+
+export interface ICreateUserResponse {
+  id: number
+  name: string
+  email: string
+  is_mailauth_completed: boolean
+  is_auto: boolean
+  is_enabled: boolean
+  is_lock: boolean
+}
