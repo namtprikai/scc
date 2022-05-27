950f9f59c2058eba0c5a0e300c0a6b80a44bc19e
tranphuongnam <phuongnam.tran@rikai.technology>
2022-05-27 11:06:22 +0700

    create search log screen

---
+++ src/views/search-log/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container log-search">
+    <el-row>
+      <el-col :span="24">
+        <log-search-period />
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <log-search-edit />
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <log-search-detail />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import LogSearchPeriod from './components/logSearchPeriod.vue'
+import LogSearchEdit from './components/logSearchEdit.vue'
+import LogSearchDetail from './components/logSearchDetail.vue'
+
+@Component({
+  name: 'LogSearch',
+  components: {
+    LogSearchPeriod,
+    LogSearchEdit,
+    LogSearchDetail
+  }
+})
+export default class LogSearch extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.el-row {
+  margin-bottom: 20px;
+}
+</style>
